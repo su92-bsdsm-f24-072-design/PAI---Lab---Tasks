fde42ba412ea9c6ecaab7f58632e5975630215e2
su92-bsdsm-f24-072-design <su92-bsdsm-f24-072@superior.edu.pk>
2026-03-08 21:59:30 +0500

    Add files via upload

---
+++ border.py
@@ -0,0 +1,15 @@
+# import cv2
+
+# # Use full path of your image
+# image = cv2.imread(r"C:\Users\Fakhar Trader\Desktop\PAI(lab)\task 5\logo.png")
+
+# if image is None:
+#     print("Image not loaded! Check filename/path")
+# else:
+#     image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+#     cv2.imshow("Bordered Image", image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+
+
